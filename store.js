5faa9c8cd77adec7eaa8648ce72f8c42c69db938
Precious Olusola <preciousolusola16@gmail.com>
2022-07-10 18:41:03 +0100

    added redux

---
+++ store.js
@@ -0,0 +1,8 @@
+import {configurationStore} from '@reduxjs/toolkit';
+import navReducer from './slices/navSlice';
+
+export const store = configureStore({
+    reducer:{
+        nav: navReducer,
+    }
+});
