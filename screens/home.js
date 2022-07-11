af6a140183b1507e4b8938d5ec01e141d4eef81d
Precious Olusola <preciousolusola16@gmail.com>
2022-07-11 02:19:33 +0100

    done with the hom screen

--- screens/home.js
+++ screens/home.js
@@ -1,7 +1,9 @@
 import { View, Text, StyleSheet, Image } from 'react-native';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import tw from 'twrnc';
-
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 // Widgets
 import SafeAreaView from '../widgets/safeArea';
@@ -9,8 +11,11 @@ import SafeAreaView from '../widgets/safeArea';
 // Components
 import NavOptions from '../components/NavOptions';
 
+// Store
+import { setDestination,setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+  const dispath = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -27,6 +32,40 @@ const HomeScreen = () => {
           }}
         />
 
+
+        <GooglePlacesAutocomplete
+          placeholder='Where From?'
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+          styles={{
+            container:{
+              flex:0
+            },
+            textInput:{
+              fontSize:18,
+            }
+          }}
+
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            // console.log(data, details);
+            dispath(setOrigin({
+              location:details.geometry.location,
+              description:data.description,
+            }))
+
+
+            dispath(setDestination(null))
+          }}
+          returnKeyType={"search"}
+          fetchDetails={true}
+          enablePoweredByContainer={false}
+          minLength={3}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
+        />
 
         <NavOptions />
       </View>
